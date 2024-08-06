394c597fdecc3d5ab04c0824646db1838774e125
Beterng <beterng@gmail.com>
2024-08-06 09:01:06 +0700

    nothing change

--- Session-1/Excersire_1.cs
+++ Session-1/Excersire_1.cs
@@ -8,6 +8,12 @@ namespace MyApplication
     {
       Console.WriteLine("Enter your age:"); // Console.Write nOT lINE
       int age = Convert.ToInt32(Console.ReadLine());
+
+      if ( age > 100 || age <= 0) {
+        Console.WriteLine("Invalid age");
+        System.Environment.Exit(1);
+      }
+
       Console.WriteLine("Your age is: " + age);
     }
   }
