edd93de020b57b23f3cd6b52dfe95b9ba79a14d1
Beterng <beterng@gmail.com>
2024-08-06 13:18:14 +0700

    update run script

--- Session-1/build.bat
+++ Session-1/build.bat
@@ -1,6 +1,2 @@
-cd c:\
-cd D:\Github\csharp-learning\Session-1
-dotnet build
-dotnet restore
 start D:\Github\csharp-learning\Session-1\run.bat
 exit
--- Session-1/run.bat
+++ Session-1/run.bat
@@ -1,1 +1,8 @@
+cd D:\Github\csharp-learning\Session-1
+dotnet build
+timeout 3
+cls
+dotnet restore
+timeout 3
+cls
 dotnet run < D:\Github\csharp-learning\Session-1\input.txt
