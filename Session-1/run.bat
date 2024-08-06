77289a23185a822acfb0e659605ff476dba220b0
Beterng <beterng@gmail.com>
2024-08-06 09:00:48 +0700

    config statup path

--- Session-1/run.bat
+++ Session-1/run.bat
@@ -1,6 +1,13 @@
+cd bin
+rmdir /s /q Debug
+cd ../obj
+rmdir /s /q Debug
+cd ..
+cls
 dotnet build
 cls
 dotnet restore
 cls
 dotnet run < ./.cache/input.txt > ./.cache/output.txt
+echo "Done!"
 exit
